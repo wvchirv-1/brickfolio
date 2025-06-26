1e5ae84953e19496249691c5d35065164e0918b9
wvchirv-1 <abbiewachira926@gmail.com>
2025-06-26 12:23:31 +0300

    initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('about/',views.about),
+
+    path('contact/',views.contact),
+    path('gallery/',views.gallery),
+    path('product/',views.product),
+
+
+]
---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"index.html")
+def about(request):
+    return render(request,"index.html")
+def contact(request):
+    return render(request,"contact.html")
+
+def gallery(request):
+    return render(request,"gallery.html")
+
+def product(request):
+    return render(request,"product.html")
